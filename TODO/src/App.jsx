28776697b32578d7c0a145a823d6a412a7fc9efe
Nishant <jainnikhil0708@gmail.com>
2025-06-21 22:22:17 +0530

    creating local storage

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { todoProvider } from "./contexts/todoContext";
 
@@ -23,6 +23,18 @@ function App() {
     setTodos((prev)=>prev.map((prevTodo)=>prevTodo===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
   }
 
+  useEffect(() => {
+    const todos=JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length){
+
+      setTodos(todos) 
+    }
+  }, [ ]);
+
+  useEffect(() => {
+    
+  localStorage.setItem("todos",JSON.stringify(todos))
+  }, [todos ]);
   return (
     <todoProvider
       values={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
