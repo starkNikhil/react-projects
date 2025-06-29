5418e4b27b4f36e69c3be7bca7f5e581495d0178
Nishant <jainnikhil0708@gmail.com>
2025-06-29 11:12:48 +0530

    configured redux

--- BlogProject/src/App.jsx
+++ BlogProject/src/App.jsx
@@ -1,15 +1,41 @@
-
-import './App.css'
+import { useEffect, useState } from "react";
+import {Outlet} from 'react-router-dom'
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
- 
- 
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-     <h1>A Blog app with appwrite</h1>
-    </>
-  )
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+         {/* { <Outlet/>} */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
