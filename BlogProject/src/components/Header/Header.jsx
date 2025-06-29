1ca6825377bf3030e962cc20651124e87d7bb39f
Nishant <jainnikhil0708@gmail.com>
2025-06-29 21:59:51 +0530

    build button and input component

--- BlogProject/src/components/Header/Header.jsx
+++ BlogProject/src/components/Header/Header.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import { Logo, Container, LogOutBtn } from "../Index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import authService from "../../appwrite/auth";
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Posts",
+      slug: "/add-posts",
+      active: authStatus,
+    },
+  ];
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((navItem) =>
+              navItem.active ? (
+                <li key={navItem.name}>
+                  <button onClick={() => navigate(navItem.slug)}>
+                    {navItem.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+
+            {authStatus&& (
+                <li>
+                   <LogOutBtn/> 
+                </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
